23c136c748d66099b50dae23d1dd4602aac6635e
Mobilszoftver laboratórium <mobsoft@aut.bme.hu>
2016-04-11 11:00:49 +0200

    Added Dagger 2 dependency

--- app/src/main/java/book/laborhazirecipe/view/FavouriteActivity.java
+++ app/src/main/java/book/laborhazirecipe/view/FavouriteActivity.java
@@ -6,9 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import book.laborhazirecipe.R;
 
 public class FavouriteActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourite);
     }
+
 }
