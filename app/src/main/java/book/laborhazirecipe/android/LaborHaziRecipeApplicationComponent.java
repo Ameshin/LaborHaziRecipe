3b2a1e071c0a14594e89d5dfa83d4469afc71c3c
Mark Barsony <bsony17@gmail.com>
2016-05-22 21:21:00 +0200

    fixed some errors

--- app/src/main/java/book/laborhazirecipe/android/LaborHaziRecipeApplicationComponent.java
+++ app/src/main/java/book/laborhazirecipe/android/LaborHaziRecipeApplicationComponent.java
@@ -5,6 +5,9 @@ import book.laborhazirecipe.interactor.RecipeInteractor;
 import book.laborhazirecipe.model.prod.ModelModule;
 import book.laborhazirecipe.network.prod.NetworkModule;
 import book.laborhazirecipe.presenter.MainPresenter;
+import book.laborhazirecipe.presenter.AboutPresenter;
+import book.laborhazirecipe.presenter.RecipeDetailsPresenter;
+import book.laborhazirecipe.presenter.FavouritePresenter;
 import book.laborhazirecipe.view.ViewModule;
 
 import javax.inject.Singleton;
@@ -17,9 +20,10 @@ public interface LaborHaziRecipeApplicationComponent {
     void inject(MainActivity mainActivity);
 
     void inject(MainPresenter mainPresenter);
-
+	void inject(AboutPresenter aboutPresenter);
+	void inject(RecipeDetailsPresenter detailPresenter);
+	void inject(FavouritePresenter favouritePresenter);
     void inject(RecipeInteractor interactor);
-
     void inject(LaborHaziRecipeApplication app);
 }
 
